e031d4f19fda20d715c4508f925ca5f8fdd419fc
lokmane rouijel <lokmane9919@gmail.com>
2025-01-30 18:26:29 +0100

    Resolve name to oid in argparse

--- ugit/cli.py
+++ ugit/cli.py
@@ -15,6 +15,8 @@ def parse_args():
     commands  = parser.add_subparsers(dest='command')
     commands.required = True
     
+    oid = base.get_oid
+    
     init_parser = commands.add_parser('init')
     init_parser.set_defaults(func=init)
     
@@ -26,14 +28,14 @@ def parse_args():
     # cat: cat file parser
     cat = commands.add_parser('cat-file')
     cat.set_defaults(func=cat_file)
-    cat.add_argument('object')
+    cat.add_argument('object', type=oid)
     
     write_tree_parser = commands.add_parser('write-tree')
     write_tree_parser.set_defaults(func=write_tree)
     
     read_tree_parse = commands.add_parser('read-tree')
     read_tree_parse.set_defaults(func=read_tree)
-    read_tree_parse.add_argument('tree')
+    read_tree_parse.add_argument('tree', type=oid)
     
     commit_parser = commands.add_parser('commit')
     commit_parser.set_defaults(func=commit)
@@ -41,16 +43,16 @@ def parse_args():
 
     log_parser = commands.add_parser('log')
     log_parser.set_defaults(func=log)
-    log_parser.add_argument('oid', nargs='?')
+    log_parser.add_argument('oid', type=oid, nargs='?')
     
     checkout_parser = commands.add_parser('checkout')
     checkout_parser.set_defaults(func=checkout)
-    checkout_parser.add_argument('oid')
+    checkout_parser.add_argument('oid', type=oid)
     
     tag_parser = commands.add_parser('tag')
     tag_parser.set_defaults(func=tag)
     tag_parser.add_argument('name')
-    tag_parser.add_argument('oid', nargs='?')
+    tag_parser.add_argument('oid', type=oid, nargs='?')
 
     return parser.parse_args()
 
